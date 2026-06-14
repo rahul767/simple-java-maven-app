d9cc01f0a5a9bff8e221fd309b1819651adb1026
rahul767 <43357647+rahul767@users.noreply.github.com>
2026-06-14 15:52:55 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello RAHUL! HELLO CHOTTU, HELLO ISHA";
+    private static final String MESSAGE = "Hello RAHUL! HELLO CHOTTU, HELLO ISHAA";
 
     public App() {}
 
